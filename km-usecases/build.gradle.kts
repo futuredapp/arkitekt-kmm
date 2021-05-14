72352fecc87d074fcfb5c07bdef852b230b30198
RudolfHladik <hladik.rudolf@gmail.com>
2021-05-14 14:50:05 +0200

    fix typo remove comment

--- km-usecases/build.gradle.kts
+++ km-usecases/build.gradle.kts
@@ -12,10 +12,6 @@ repositories {
 }
 
 kotlin {
-    /* Targets configuration omitted. 
-    *  To find out how to configure the targets, please follow the link:
-    *  https://kotlinlang.org/docs/reference/building-mpp-with-gradle.html#setting-up-targets */
-
     android {
         publishAllLibraryVariants()
     }
